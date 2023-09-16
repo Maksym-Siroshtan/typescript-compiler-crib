cccd8aca5337c705e5ea59a7e335919a2e349c76
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-16 15:31:05 +0300

    Strict regime

--- build/app.js
+++ build/app.js
@@ -1,2 +1,23 @@
 "use strict";
-const a = 5;
+function test(n) {
+    return n;
+}
+// let f: StrOrNum = test;
+class A {
+    getB() {
+        return function () {
+            // this.b = 1; Этот контейнер затемняет внешнее значение "this".
+        };
+    }
+}
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+function createUser(user) {
+    //.....
+    const defaultUser = new User("default");
+    console.log(user);
+    console.log(defaultUser);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-const a: number = 5;
+function test(n: number): number {
+  return n;
+}
+
+type StrOrNum = (n: string | number) => number;
+
+// let f: StrOrNum = test;
+
+class A {
+  b: number;
+
+  getB() {
+    return function () {
+      // this.b = 1; Этот контейнер затемняет внешнее значение "this".
+    };
+  }
+}
+
+class User {
+  constructor(public name: string) {}
+}
+
+function createUser(user: User) {
+  //.....
+  const defaultUser = new User("default");
+  console.log(user);
+  console.log(defaultUser);
+}
